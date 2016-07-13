81639b6af5029fc52299c93827e39ed6b7c4df56
Jiang Liyin <liyinjiang@cienet.com.cn>
2016-07-13 14:45:36 +0800

    Add public head in framework.h

--- KBPopupBubbleFramework/KBPopupBubbleFramework.h
+++ KBPopupBubbleFramework/KBPopupBubbleFramework.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char KBPopupBubbleFrameworkVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <KBPopupBubbleFramework/PublicHeader.h>
 
 
+#import <KBPopupBubbleFramework/KBPopupBubbleView.h>
